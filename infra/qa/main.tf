1386a1b3c50b8c6adad0fb578dd62e01b9110046
CheffoPiu <danilo@evolutivos.net>
2025-06-24 13:11:39 -0500

    fix(qa-infra): correct egress block syntax in security group

--- infra/qa/main.tf
+++ infra/qa/main.tf
@@ -26,6 +26,11 @@ resource "aws_security_group" "mongo_sg" {
     protocol        = "tcp"
     security_groups = [var.auth_sg_id]
   }
-  egress { from_port = 0; to_port = 0; protocol = "-1"; cidr_blocks = ["0.0.0.0/0"] }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
