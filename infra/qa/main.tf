72e7bb67fdeb25d8de09d869824bfa60d7f38a6a
CheffoPiu <danilo@evolutivos.net>
2025-06-24 16:04:58 -0500

    fix(qa-infra): declare aws_instance.mongo before volume attachment

--- infra/qa/main.tf
+++ infra/qa/main.tf
@@ -1,3 +1,7 @@
+provider "aws" {
+  region = var.aws_region
+}
+
 terraform {
   backend "local" {}
 }
@@ -6,8 +10,6 @@ data "aws_subnet" "selected" {
   id = var.subnet_id
 }
 
-provider "aws" { region = var.aws_region }
-
 # 1) EBS volume
 resource "aws_ebs_volume" "mongo_data" {
   availability_zone = data.aws_subnet.selected.availability_zone
@@ -15,30 +17,36 @@ resource "aws_ebs_volume" "mongo_data" {
   tags = { Name = "mongo-data-qa" }
 }
 
-# 2) Attach volume to the instance (created below)
+# 2) EC2 instance for MongoDB
+resource "aws_instance" "mongo" {
+  ami                         = var.ami
+  instance_type               = var.instance_type
+  subnet_id                   = var.subnet_id
+  key_name                    = var.ssh_key_name
+  vpc_security_group_ids      = [aws_security_group.mongo_sg.id]
+  associate_public_ip_address = false
+
+  user_data = <<-EOF
+    #!/bin/bash
+    # …tu script…
+  EOF
+
+  tags = { Name = "qa-mongo" }
+}
+
+# 3) Attach the EBS volume to the instance
 resource "aws_volume_attachment" "mongo_attach" {
   device_name = "/dev/xvdf"
   volume_id   = aws_ebs_volume.mongo_data.id
   instance_id = aws_instance.mongo.id
 }
 
-# 3) Security Group allowing only your auth-service SG to talk on 27017
+# 4) Security group…
 resource "aws_security_group" "mongo_sg" {
-  name        = "qa-mongo-sg"
-  description = "Allow auth-service to access Mongo"
-  vpc_id      = var.vpc_id
-
-  ingress {
-    from_port       = 27017
-    to_port         = 27017
-    protocol        = "tcp"
-    security_groups = [var.auth_sg_id]
-  }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  # …
 }
 
+# 5) Output Mongo URI
+output "mongo_uri" {
+  value = "mongodb://${var.mongo_root_user}:${var.mongo_root_pass}@${aws_instance.mongo.private_ip}:27017/league-auth?authSource=admin"
+}
--- infra/qa/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "service_url" {
-  description = "URL of the QA load balancer"
-  value       = module.qa_mongo.lb_dns_name
-}
